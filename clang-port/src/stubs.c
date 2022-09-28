e1d1e3d72cb33f882be09b70590b38b1aaaa16ad
Yuhan Deng <yhdeng@stanford.edu>
2022-09-28 23:19:34 +0000

    Add stubs for imported env functions

---
+++ clang-port/src/stubs.c
@@ -0,0 +1,25 @@
+#include <unimplemented.h>
+
+extern int __wasilibc_dttoif( int ); 
+extern int setjmp( void* );
+
+int _Z17__wasilibc_dttoifi( int );
+int _Z17__wasilibc_dttoifi( int a ) {
+  return __wasilibc_dttoif( a );
+}
+
+int _Z6setjmpPv( void* );
+int _Z6setjmpPv( void* addr ) {
+  return setjmp( addr );
+}
+
+int posix_madvise(void *, int, int);
+int posix_madvise(void *addr, int len, int advise) {
+  return 0;
+}
+
+int dup2(int, int);
+int dup2(int a, int b) {
+  (void)unimplemented(58);
+  return -1;
+}
